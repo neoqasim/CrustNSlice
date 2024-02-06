1d3cedd37d1fa4367e6a47c41550a651a620a128
neoqasim <neoqasim@gmail.com>
2024-02-06 22:04:39 +0500

    minor footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     return (
         <div className='max-w-[1600px] mx-auto px-4 py-8'>
             <footer className="flex flex-col items-center justify-center bg-white text-black py-6 rounded-lg shadow-md">
-                <p className="text-lg font-bold mb-4">Made with <span className="text-orange-600">love</span> by neoQasim</p>
+                <p className="text-xl font-bold mb-4">Created with <span className="text-orange-600 text-2xl"> dedication
+                </span>by <span className='bg-orange-600 text-white text-2xl px-1.5 py-1 rounded'>  NeoQasim</span></p>
                 <div className="flex space-x-4">
                     <a href="https://github.com/neoqasim" target="_blank" rel="noopener noreferrer">
                         <FaGithub className="text-gray-600 hover:text-orange-600" size={32} />
